a8c89fc1e28126f7cdd9329dae3feb8b3753713a
y.uchizono <ynetlabo@gmail.com>
2024-01-21 19:53:59 +0900

    v20240121-01

--- ynetlabo-app/plugins/06.analytics.client.ts
+++ ynetlabo-app/plugins/06.analytics.client.ts
@@ -9,7 +9,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(
     VueGtag,
     {
-      appName: "wari-web", // サイトの名称
+      appName: "Y.netLabo-web", // サイトの名称
       pageTrackerScreenviewEnabled: true, // ページトラッキングスクリーンビューを有効
       config: { id: `G-LSBSP801C4` }, // GoogleAnalytics(GA4)の測定IDを指定する
     },
